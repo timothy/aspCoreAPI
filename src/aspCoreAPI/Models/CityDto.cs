ba37f94fabc8b6d9308b7bf0e0067d26df3363a3
timothy <tabradford@liberty.edu>
2017-01-18 16:18:24 -0500

    Simple Implementation until Entity Framework is implemented.

--- src/aspCoreAPI/Models/CityDto.cs
+++ src/aspCoreAPI/Models/CityDto.cs
@@ -7,5 +7,9 @@ namespace aspCoreAPI.Models
 {
     public class CityDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfPointsOfInterest { get; set; }
     }
 }
