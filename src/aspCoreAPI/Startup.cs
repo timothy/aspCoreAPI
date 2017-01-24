e8a81ad3f1936be108caa15c270a89eb9288ca0f
timothy <tabradford@liberty.edu>
2017-01-24 14:06:55 -0500

    added xml return capability

--- src/aspCoreAPI/Startup.cs
+++ src/aspCoreAPI/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 //using Newtonsoft.Json.Serialization;
@@ -17,7 +18,7 @@ namespace aspCoreAPI
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));//To change return data //Header - Accept application/xml //otherwise - Header -  application/json
 /*                .AddJsonOptions(o =>
                             {
                                 if (o.SerializerSettings.ContractResolver != null)
